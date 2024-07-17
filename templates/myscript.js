bb1365b464b08a77ba5192428243a0a94e54eeb2
Dipanshu <94486124+Dipanshu-04@users.noreply.github.com>
2024-07-17 22:24:35 +0000

    added translucent navbar

---
+++ templates/myscript.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('.navbar');
+  if(window.scrollY > 0){
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
