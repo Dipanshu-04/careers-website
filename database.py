d07e56569df80e5f38674291680f5bf3dfa11807
Dipanshu <94486124+Dipanshu-04@users.noreply.github.com>
2024-08-22 06:32:53 +0000

    Added multiple improvements

--- database.py
+++ database.py
@@ -30,3 +30,15 @@ def load_job_from_db(id):
   else:
     return result
 
+def add_application_to_db(job_id, data):
+  cursor = connection.cursor()
+  query = ("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+  cursor.execute(query, 
+                 job_id=job_id, 
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])
+
